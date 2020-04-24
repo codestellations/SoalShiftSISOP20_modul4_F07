2f8f7017f02985da7b0d16ad8119dae46651f271
codestellations <erlindaargyt@gmail.com>
2020-04-24 16:54:23 +0700

    no 1 fixed typo

--- ssfs.c
+++ ssfs.c
@@ -15,7 +15,7 @@
 
 #define KEY 10
 
-static const char *dirpath = "/home/el/tiga";
+static const char *dirpath = "/home/el/Documents";
 char charlist[1024] = "9(ku@AW1[Lmvgax6q`5Y2Ry?+sF!^HKQiBXCUSe&0M.b%rI'7d)o4~VfZ*{#:}ETt$3J-zpc]lnh8,GwP_ND|jO";
 char *limit;
 
